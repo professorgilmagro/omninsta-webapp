d188ab70532d630c592cc275312bbd4fcc7c311f
Gilmar Soares <gilmar.santos@gmmti.com.br>
2019-08-07 12:48:58 -0300

    Adiciona as variaveis de ambiente para configuraçao da API

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const config = {
-	host: '10.86.52.104',
-	port: 3333,
+	host: process.env.API_HOST || 'http://10.86.52.104',
+	port: process.env.API_PORT || 3333,
 	getURL(...pathSegments) {
 		let path = '';
 		if (pathSegments.length > 0) {
 			path = '/' + pathSegments.join('/', path);
 		}
 
-		return `http://${this.host}:${this.port}${path}`;
+		return `${this.host}:${this.port}${path}`;
 	}
 };
 
